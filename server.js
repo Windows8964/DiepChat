e19a7e1192342137239ad5994b5b095f45b75149
Windows8964 <63546466+Windows8964@users.noreply.github.com>
2021-05-04 19:52:19 +0300

    cleaner code and better id system ig?

--- server.js
+++ server.js
@@ -1,49 +1,61 @@
-const WebSocket = require('ws')
+const WebSocket = require('ws');
 const fs = require('fs');
 
-process.on('uncaughtExeption', console.log)
-const wss = new WebSocket.Server({ port: 8080 });
+process.on('uncaughtExeption', console.log);
+const wss = new WebSocket.Server({
+  port: 8080
+});
 let creds = require('./creds.json');
-
+class IdGenerator {
+  constructor() {
+    this.id = 0;
+  }
+  generateId() {
+    return this.id++;
+  }
+}
+const webSocketIdGenerator = new IdGenerator();
 wss.on('connection', ws => {
-  ws.id = Math.random().toString(16).substr(2);
+  ws.id = webSocketIdGenerator.generateId();
   console.log('connection');
   ws.on('message', (msg) => {
     if (typeof msg !== 'string') return ws.close(1020, 'you are bad');
-    try
-    {
+    try {
       msg = JSON.parse(msg);
-    }
-    catch
-    {
+    } catch {
       return ws.close(1020, 'you are still bad');
     }
     if (!(msg instanceof Object)) return ws.close(1020, 'you are really bad');
     console.log(msg);
-    switch (msg.type)
-    {
-//      case 'create': return signup(ws, msg, ws._socket.remoteAddress);
-//      case 'login': return login(ws, msg);
-      case 'message': { sendToAll(ws, msg); return; }
-      case 'join' :  { ws.server = msg.url, ws.name = (msg.name || 'Unnamed') + '#' + Math.random().toString(16).substr(11); return; }
+    switch (msg.type) {
+    /*case 'create': return signup(ws, msg, ws._socket.remoteAddress);
+      case 'login': return login(ws, msg);*/
+      case 'message': {
+        sendToAll(ws, msg);
+        return;
+      }
+      case 'join': {
+        ws.server = msg.url;
+        ws.name = (msg.name || 'Unnamed') + '#' + ws.id;
+        return;
+      }
     }
   });
 });
 
 let last = 0;
-function sendToAll(ws, data)
-{
+
+function sendToAll(ws, data) {
   if (!data.message) return;
-//  if (0 - (last - (last=Date.now())) >= 100) return ws.send(JSON.stringify({
-//    type: 'RATELIMITED',
-//    data: 'Please try again. you have been ratelimited'
-//  }))
+/*if (0 - (last - (last=Date.now())) >= 100) return ws.send(JSON.stringify({
+  type: 'RATELIMITED',
+    data: 'Please try again. you have been ratelimited'
+  }))*/
   const client = Array.from(wss.clients);
-  for (let i = 0; i < client.length; i++)
-  {
+  for (let i = 0; i < client.length; i++) {
     if ((client[i].readyState === WebSocket.OPEN) && (client[i].server === ws.server)) client[i].send(JSON.stringify({
       type: 'message',
-      data: data.message+'',
+      data: data.message + '',
       from: ws.name
     }));
   }
@@ -51,7 +63,10 @@ function sendToAll(ws, data)
 
 function login(ws, credentials) {
   console.log('sign in attempt');
-  const { username, password } = credentials;
+  const {
+    username,
+    password
+  } = credentials;
   const cred = creds[username];
   if (!cred) return ws.close();
   if (password === cred.password) {
@@ -59,12 +74,13 @@ function login(ws, credentials) {
     ws.send(cred.token);
   }
 }
+
 function signup(ws, credentials, ip) {
   if (creds[credentials.username]) return;
   console.log('new user');
   console.log(credentials);
   creds[credentials.username] = {};
   creds[credentials.username]['password'] = credentials.password;
-  creds[credentials.username]['token'] = (Math.random()* +ip.split('.')[3]).toString(16).substr(2);
+  creds[credentials.username]['token'] = (Math.random() * +ip.split('.')[3]).toString(16).substr(2);
   fs.writeFileSync('./creds.json', JSON.stringify(creds, null, 2));
 }
